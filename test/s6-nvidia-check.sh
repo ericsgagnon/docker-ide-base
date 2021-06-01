d23f6c8a73501002ef8291017b8dcd20fdfb96a0
Eric <ericsgagnon@gmail.com>
2021-06-01 02:22:45 -0400

    Remove test build

--- test/s6-nvidia-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "------------------------------------------------"
-echo "check to make sure the container can access gpus"
-echo "any gpus currently accessible are listed here:  "
-nvidia-smi -L
-echo "------------------------------------------------"
