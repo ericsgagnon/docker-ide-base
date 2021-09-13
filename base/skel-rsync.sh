12d7ef826c11bef97eafcb4c8c919514247cd0b2
Eric <ericsgagnon@gmail.com>
2021-09-13 00:25:28 -0400

    Revert skel-rsync.sh to avoid stdout

--- base/skel-rsync.sh
+++ base/skel-rsync.sh
@@ -4,9 +4,10 @@
 # when logging in. It won't overwrite existing files, but 
 # will copy deleted files if they exist in /etc/skel
 
-echo "
------------------------------------------------------------------------------------------------
-copy /etc/skel to ${HOME}, skipping existing files (this will not overwrite any existing files)
------------------------------------------------------------------------------------------------
- " \
-&& rsync -rltD --ignore-existing /etc/skel/ ${HOME}/
+# echo "
+# -----------------------------------------------------------------------------------------------
+# copy /etc/skel to ${HOME}, skipping existing files (this will not overwrite any existing files)
+# -----------------------------------------------------------------------------------------------
+#  " \
+# && 
+rsync -rltD --ignore-existing /etc/skel/ ${HOME}/
